547de57a5db702b3c94375636e4e791e317f8b1d
aegiru <104532735+aegiru@users.noreply.github.com>
2022-06-02 01:50:41 +0200

    Final update.

--- ezbackup.sh
+++ ezbackup.sh
@@ -2,6 +2,7 @@
 
 UNIQUE_SEPARATOR="😎"
 CONNECTION_LOCATION="./connections"
+KEY_LOCATION="./keys"
 
 
 
@@ -69,6 +70,21 @@ reset_connection_variable() {
 }
 
 
+reset_load() {
+    CURRENTLY_LOADED=""
+}
+
+
+
+no_connection() {
+    dialog --stdout --infobox "No connection. Please set up one first." 13 60
+
+    check_for_cancel_in_dialog
+
+    main_menu
+}
+
+
 
 download_and_decompress() {
     FOLDER_NAME_SCP="${FOLDER_NAME// /\\ }"
@@ -76,7 +92,7 @@ download_and_decompress() {
     if [ -z keyname ] ; then
         sshpass -p "$password" scp "$username@$address:$remotelocation/$FOLDER_NAME_SCP" "./"
     else
-        scp -i "$keyname" "$username@$address:$remotelocation/$FOLDER_NAME_SCP" "./"
+        scp -i "$KEY_LOCATION/$keyname" "$username@$address:$remotelocation/$FOLDER_NAME_SCP" "./"
     fi
 
     tar --overwrite -C "$locallocation" -xf  "./$FOLDER_NAME" 
@@ -90,7 +106,7 @@ list_folders() {
     if [ -z keyname ] ; then
         FILELIST=$(sshpass -p "$password" ssh "$username@$address" "cd "$remotelocation" && find ./* -maxdepth 0 -type f -iname '*.tar.gz' -printf '%f$UNIQUE_SEPARATOR'")
     else
-        FILELIST=$(ssh -i "$keyname" "$username@$address" "cd "$remotelocation" && find ./* -maxdepth 0 -type f -iname '*.tar.gz' -printf '%f$UNIQUE_SEPARATOR'")
+        FILELIST=$(ssh -i "$KEY_LOCATION/$keyname" "$username@$address" "cd "$remotelocation" && find ./* -maxdepth 0 -type f -iname '*.tar.gz' -printf '%f$UNIQUE_SEPARATOR'")
     fi
 
     install_unique_separator
@@ -118,6 +134,52 @@ list_folders() {
 
 
 
+save_current_selection() {
+    TEMP_LOADED=$CURRENTLY_LOADED
+}
+
+
+
+reinstate_previous_selection() {
+    CURRENTLY_LOADED=$TEMP_LOADED
+}
+
+
+
+no_loaded() {
+    NO_LOAD=$(dialog --stdout --msgbox "No connection selected! Please create and select a connection first." 13 60)
+
+    check_for_cancel_in_dialog
+}
+
+
+
+check_if_loaded() {
+    if [ -z "$CURRENTLY_LOADED" ] ; then
+        no_loaded
+
+        main_menu
+    fi
+}
+
+
+
+backup_succ() {
+    B_SUCC=$(dialog --stdout --msgbox "[ $DATE.tar.gz ] created!" 13 60)
+
+    check_for_cancel_in_dialog
+}
+
+
+
+restore_succ() {
+    R_SUCC=$(dialog --stdout --msgbox "[ $DATE.tar.gz ] removed!" 13 60)
+
+    check_for_cancel_in_dialog
+}
+
+
+
 compress_and_upload() {
     DATE=$(date '+%Y-%m-%d %H-%M-%S')
     tar -C "$locallocation" -czf "$DATE.tar.gz" $(ls "$locallocation")
@@ -125,7 +187,7 @@ compress_and_upload() {
     if [ -z keyname ] ; then
         sshpass -p "$password" scp "./$DATE.tar.gz" "$username@$address:$remotelocation"
     else
-        scp -i "$keyname" "./$DATE.tar.gz" "$username@$address:$remotelocation"
+        scp -i "$KEY_LOCATION/$keyname" "./$DATE.tar.gz" "$username@$address:$remotelocation"
     fi
 
     rm "./$DATE.tar.gz"
@@ -136,6 +198,11 @@ compress_and_upload() {
 fix_connection_newline() {
     display="${display//[$'\t\r\n']}"
     address="${address//[$'\t\r\n']}"
+    username="${username//[$'\t\r\n']}"
+    password="${password//[$'\t\r\n']}"
+    keyname="${keyname//[$'\t\r\n']}"
+    locallocation="${locallocation//[$'\t\r\n']}"
+    remotelocation="${remotelocation//[$'\t\r\n']}"
 }
 
 
@@ -166,9 +233,9 @@ connection_config() {
 
 
 
-load_connections() {
+load_existing_connections() {
     pushd $CONNECTION_LOCATION
-    CONNECTIONS=$(find ./* -maxdepth 0 -type f -iname "*.connection" -printf "%f$UNIQUE_SEPARATOR")
+    CONNECTIONS=$(find ./* -maxdepth 0 -type f -iname "*" -printf "%f$UNIQUE_SEPARATOR")
     popd
 
     install_unique_separator
@@ -179,7 +246,23 @@ load_connections() {
 
 
 filename_selector() {
-    FILENAME_DIALOG="dialog --stdout --ok-label \"Submit\" --inputbox \"Choose a filename to save your connection to.\" 12 60 0"
+    FILENAME_DIALOG=$(dialog --stdout --ok-label "Submit" --inputbox "Choose a filename to save your connection to." 12 60 0)
+
+    check_for_cancel_in_dialog
+}
+
+
+
+save_connection() {
+    TEXT_TO_SAVE="display=\"$display\"
+address=\"$address\"
+username=\"$username\"
+password=\"$password\"
+keyname=\"$keyname\"
+locallocation=\"$locallocation\"
+remotelocation=\"$remotelocation\""
+
+    echo "$TEXT_TO_SAVE" > "$CONNECTION_LOCATION/$SAVE_FILENAME"
 }
 
 
@@ -191,39 +274,108 @@ new_connection() {
 
     filename_selector
 
-    TEXT_TO_SAVE="display=\"$display\"
-    address=\"$address\"
-    username=\"$username\"
-    password=\"$password\"
-    keyname=\"$keyname\"
-    locallocation=\"$locallocation\"
-    remotelocation=\"$remotelocation\""
+    SAVE_FILENAME=$FILENAME_DIALOG
 
-    echo "$TEXT_TO_SAVE" > "$CONNECTION_LOCATION/$FILENAME_DIALOG"
+    save_connection
 
     reset_connection_variable
+
+    connection_choice
+}
+
+
+
+remove_connection() {
+    rm "$CONNECTION_LOCATION/$CONNECTION_TO_DELETE"
+}
+
+
+
+load_connection() {
+    . "$CONNECTION_LOCATION/$CURRENTLY_LOADED"
+
+    fix_connection_newline
 }
 
 
 
 connection_selector() {
-    load_connections
+    load_existing_connections
 
     CONNECTIONS_DIALOG="dialog --stdout --ok-label \"Submit\" --menu \"Choose the connection.\" 12 60 0 "
 
-    d=1
+    d=0
 
     for i in "${ARRAY_CONNECTIONS[@]}"
     do
         . "$CONNECTION_LOCATION/$i"
         fix_connection_newline
-        CONNECTIONS_DIALOG+="$d \"$display | $address\" "
-        (($d++))
+
+        LOADED_INDICATOR=" "
+
+        if [[ "$i" == "$CURRENTLY_LOADED"  ]] ; then
+            LOADED_INDICATOR="X"
+        fi
+
+        CONNECTIONS_DIALOG+="$d \"[$LOADED_INDICATOR] $display | $address\" "
+        ((d++))
     done
 
     CONNECTIONS_VALUE=$(eval $CONNECTIONS_DIALOG)
 
     check_for_cancel_in_dialog
+
+    CURRENTLY_LOADED=${ARRAY_CONNECTIONS[$CONNECTIONS_VALUE]}
+}
+
+
+
+use_connection() {
+    connection_selector
+
+    reset_connection_variable
+
+    load_connection
+
+    connection_choice
+}
+
+
+
+edit_connection() {
+    save_current_selection
+
+    connection_selector
+
+    reset_connection_variable
+
+    load_connection
+
+    connection_config
+
+    SAVE_FILENAME=$CURRENTLY_LOADED
+
+    save_connection
+
+    reinstate_previous_selection
+
+    connection_choice
+}
+
+
+
+delete_connection() {
+    save_current_selection
+    
+    connection_selector
+
+    CONNECTION_TO_DELETE=$CURRENTLY_LOADED
+
+    remove_connection
+
+    reinstate_previous_selection
+
+    connection_choice
 }
 
 
@@ -231,24 +383,30 @@ connection_selector() {
 connection_menu() {
     CONNECTION_MENU_DIALOG=$(dialog --stdout --ok-label "Submit" \
     --menu "What would you like to do with your connections?" 12 60 0 \
-    1 "New Connection" \
-    2 "Edit Existing Connections" \
-    3 "Delete a Connection")
+    1 "Choose a Connection" \
+    2 "New Connection" \
+    3 "Edit Existing Connections" \
+    4 "Delete a Connection" \
+    5 "Return to the Main Menu")
 
     check_for_cancel_in_dialog
 }
 
 
 
-connection_config() {
+connection_choice() {
     connection_menu
 
     if [ $CONNECTION_MENU_DIALOG -eq 1 ] ; then
+        use_connection
+    elif [ $CONNECTION_MENU_DIALOG -eq 2 ] ; then
         new_connection
-    elif [ $CONNECTION_MENU_DIALOG -eq 2] ; then
-
     elif [ $CONNECTION_MENU_DIALOG -eq 3 ] ; then
-
+        edit_connection
+    elif [ $CONNECTION_MENU_DIALOG -eq 4 ] ; then
+        delete_connection
+    elif [ $CONNECTION_MENU_DIALOG -eq 5 ] ; then
+        main_menu
     else
         main_menu
     fi
@@ -257,24 +415,28 @@ connection_config() {
 
 
 upload_config() {
-    connection_config
+    check_if_loaded
 
     compress_and_upload
+
+    backup_succ
+
+    main_menu
 }
 
 
 
 download_config() {
-    connection_config
+    check_if_loaded
 
     list_folders
+
+    main_menu
 }
 
 
 
 main_menu() {
-    reset_connection_variable
-
     MENU_VALUES=$(dialog --stdout --ok-label "Submit" \
     --menu "Back up or restore?" 12 60 0 \
     1 "Back Up" \
@@ -287,12 +449,12 @@ main_menu() {
 
     if [ $MENU_VALUES -eq 1 ] ; then
         upload_config
-    elif [ $MENU_VALUES -eq 4 ] ; then
-        leave
     elif [ $MENU_VALUES -eq 2 ] ; then
         download_config
     elif [ $MENU_VALUES -eq 3 ] ; then
-        connection_selector
+        connection_choice
+    elif [ $MENU_VALUES -eq 4 ] ; then
+        leave
     else
         main_menu
     fi
@@ -300,4 +462,14 @@ main_menu() {
 
 
 
-main_menu
+begin() {
+    reset_connection_variable
+
+    reset_load
+
+    main_menu
+}
+
+
+
+begin
